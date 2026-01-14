197c5d9c84bd7fa7777a7a33c6fd19641d6ad395
Ilyan khan <ilyaankhan342@gmail.com>
2026-01-14 20:48:17 +0500

    Add introductory print statements and user input

---
+++ week 1/class 1.py
@@ -0,0 +1,10 @@
+print("Ilyan khan,","20 years old,","I love girls.")
+print("Hello YOU")
+name="Ilyan khan"
+age=19
+print("My name is",name,"and my age is",age)
+name1= input("Enter your name: ")
+age1= int(input("Enter your age: "))
+print("My name is",name1,"and my age is",age1,"years old.")
+print("My name is "+name1+" and my age is "+str(age1)+" years old.")
+print(type(name),"\n",type(age))
