a7c6a6c7d615c6b4366b0ce61fa044ef731af127
Ilyan321 <ilyaankhan342@gmail.com>
2026-01-20 14:51:18 +0500

    for loops

---
+++ class_2_for_loop.py
@@ -0,0 +1,39 @@
+# for i in range (1,91,4):
+#     print(i)
+
+# #    loop on string
+
+# char = "Ilyan khan"
+# for i in char:
+#     print(i,end="")
+
+
+# #    loop on list
+# list=["apple","banana","mango","grapes","orange"]
+# for i in list:
+#     print(i,end=" ")
+
+list1=[1,2,3,4,5,6]
+print("Sum of list is:",sum(list1))
+total=0
+for i in list1:
+    total+=i
+print("Sum of list using loop is:",total)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
