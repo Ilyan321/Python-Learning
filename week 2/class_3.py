608fc956eef2e1ac45fb5f85d7b16af07132a8f0
Ilyan321 <ilyaankhan342@gmail.com>
2026-01-21 14:26:45 +0500

    week 2 class 3 loops

--- week 2/class_3.py
+++ week 2/class_3.py
@@ -1,3 +1,8 @@
+print("Even Numbers: ")
 for i in range(1,21):
     if i%2==0:
-        print(i)
+        print(i,end=" ")
+print()
+print("Even Numbers: ")
+for i in range (2,21,2):
+    print (i,end=' ')
