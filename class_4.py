5e75e8aecf775f3ccbff5a1509fecdd0bba908ec
Ilyan321 <ilyaankhan342@gmail.com>
2026-01-22 14:55:21 +0500

    Class 4 Codes

--- class_4.py
+++ class_4.py
@@ -1,3 +1,12 @@
-fruits=["apple", "banana", "orange"]
-fruits.remove("banana")
-print(fruits)
+# # removing an element from list
+
+# fruits=["apple", "banana", "orange"]
+# fruits.remove("banana")
+# print(fruits)
+
+#  printing index along with element using for loop
+list=[1, 2, 3, 4, 5]
+index=0
+for i in list:
+    print(index, " ", i)
+    index+=1
