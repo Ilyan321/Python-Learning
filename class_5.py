d5d6be02ee39b75291d4766dd6153b523d23290d
Ilyan321 <ilyaankhan342@gmail.com>
2026-01-23 15:02:40 +0500

    Class 5 Programs

--- class_5.py
+++ class_5.py
@@ -2,13 +2,16 @@ num1=int(input("Enter first number: "))
 num2=int(input("Enter second number: "))
 
 for i in range(num1,num2+1):
-    if i>2:
+    if i>1:
+        is_prime=True
+        for j in range (2,i):
+    
+            if i%j==0:
+                is_prime=False
+                break
 
-        if i%num1==0:
-            continue
-        else:
+        if is_prime:
             print(i,"is a prime number")
-    else:
-        print(i,"is a prime number")
+           
             
     
