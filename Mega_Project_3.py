98f3ef8656291d8d11fb244158adb0940c7b3533
Ilyan321 <ilyaankhan342@gmail.com>
2026-01-23 16:04:34 +0500

    Mega Project 3

--- Mega_Project_3.py
+++ Mega_Project_3.py
@@ -1,1 +1,17 @@
 import random
+score=int(0)
+print("---------------Number Guessing Game---------------")
+for i in range (3):
+    guess=random.randint(1,10)
+    print("The computer guessed:",guess)
+
+    num=int(input("Enter a number between 1 and 10: "))
+    
+    if num==guess:
+        print("You guessed correctly!  10 Score added.")
+        score+=10
+        print("Your total score is:",score)
+    else:
+        print("You guessed incorrectly!")
+    print()  
+print("----------Your final score is:",score)
