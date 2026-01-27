e99e22fbe0462943969b326786240cb2476dcc74
Ilyan321 <ilyaankhan342@gmail.com>
2026-01-27 15:06:25 +0500

    functions practice

--- week 3/class_2_fuctions.py
+++ week 3/class_2_fuctions.py
@@ -1,6 +1,25 @@
-def outer():
-    print("A")
-    def inner():
-        print("B")
-    inner()
-outer()
+# # inner and outer functions
+# def outer():
+#     print("A")
+#     def inner():
+#         print("B")
+#     inner()
+# outer()
+
+# calculator function
+def calculator (num1,num2,op):
+    if op == "+":
+        return num1 + num2
+    elif op == "-":
+        return num1 - num2
+    elif op == "*":
+        return num1 * num2
+    elif op == "/":
+        return num1 / num2
+    elif op =="%":
+        return num1 % num2
+    else:
+        return "Invalid operation"
+    
+result = calculator(4,2,"%")
+print(result)
