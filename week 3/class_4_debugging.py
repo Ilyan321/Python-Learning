8f294bbf2d486571026f47b0c1c0748399d4de3d
Ilyan321 <ilyaankhan342@gmail.com>
2026-01-30 09:57:23 +0000

    Class 4 debugging

---
+++ week 3/class_4_debugging.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    print("Adding", a, "and", b)
+    result = a + b
+    print("Result is", result)
+    return result
+
+
+print(2)(2)
+add(2, 3)
--- week 3/class_4_try-except.py
+++ week 3/class_4_try-except.py
@@ -1,6 +1,31 @@
-try:
-    print(x)
-except NameError:
-    print("variable not defined")
-except:
-    print("something else went wrong")
+# All errors
+# ZeroDivisionError
+# NameError
+# IndexError
+# KeyError
+# TypeError
+# ValueError
+# FileNotFoundError
+# ImportError
+# AttributeError
+# IndentationError
+# SyntaxError
+
+
+# try:
+#     print(x)
+# except NameError:
+#     print("variable not defined")
+# except :
+#     print("indentation error occurred")
+# finally:
+#     print("the 'try except' is finished")
+
+def name(name):
+    if type(name) != str:
+        raise TypeError ("Only strings are allowed")
+    
+try: 
+    name(123)
+except TypeError as e:
+    print(e)
