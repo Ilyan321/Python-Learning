76a5c3f6d7510ccbfd3863903aaa1c209c4c9c1c
Ilyan321 <ilyaankhan342@gmail.com>
2026-02-16 14:49:33 +0500

    class_2.py

--- week 5/class_2.py
+++ week 5/class_2.py
@@ -1,12 +1,27 @@
 class Person:
-    def __init__(self,name,age):
+    name=""
+    age=0
+    def __init__(self,name,age):    #constructur
         self.name=name
         self.age=age
     
-    def greet(self):
+    def greet(self):     #to greet 
         print("Hello",self.name)
 
+    def display(self):   #to display or print 
+        print(self.name,self.age)
+    # def display1():
+    #     print(name,age)
 
 p1=Person("Ilyan",19)
-print(p1.name,p1.age)
+p2=Person("Damon",165)
+p1.display()
+p2.display()
 p1.greet()
+p2.greet()
+
+# p3=Person
+# p3.name="Elijah"
+# p3.age=1065
+# print(p3.name,p3.age)
+# p3.greet()
