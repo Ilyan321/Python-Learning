3e5d19af5d0273abae8ee8f3533538ff6a5675f0
Ilyan321 <ilyaankhan342@gmail.com>
2026-02-18 14:42:35 +0500

    Created class_4.py for Interitance learning

--- week 5/class_4.py
+++ week 5/class_4.py
@@ -7,13 +7,16 @@ class Person:
 
     def display(self):
         print(self.name,self.age)
+class Ilyan(Person):       #inherited fron Person
 
-class Ilyan(Person):
+    def __init__(self,name,age,height):
+        super().__init__(name,age)
+        self.height=height
     def display(self):
-        print("Fron ilyan class",self.name,self.age)
+        print("Fron ilyan class",self.name,self.age,self.height)
     
 
 p1=Person("Ilyan",19)
 p1.display()
-p2=Ilyan("Damon",165)
+p2=Ilyan("Damon",165,7)
 p2.display()
