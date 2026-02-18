4ee7a2eb8b524ae3f9c6b8f3e38f0e66c0f823ed
Ilyan321 <ilyaankhan342@gmail.com>
2026-02-18 14:56:16 +0500

    Learned overloading of functions

--- week 5/class_4.py
+++ week 5/class_4.py
@@ -10,20 +10,20 @@ class Person:
 class Ilyan(Person):       #inherited fron Person
 
     def __init__(self,name,age,height):
-        super().__init__(name,age)
+        super().__init__(name,age)     # Super() tells it to run 1st parent function first
         self.height=height
     def display(self):
         print("Fron ilyan class",self.name,self.age,self.height)
 
 class Student(Person):
-    def display(self):
-        print("From Student class",self.name,self.age)
+    def display(self,greet="Hello"):
+        print(greet,"From Student class",self.name,self.age)
 
     
 
 # p1=Person("Ilyan",19)
 # p1.display()
-# p2=Ilyan("Damon",165,7)
-# p2.display()
+p2=Ilyan("Damon",165,7)
+p2.display()
 p3=Student("Ilyan",19)
 p3.display()
