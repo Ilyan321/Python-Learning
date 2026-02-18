ac7f04d91b11149f70520a1506bee310e01d5c8f
Ilyan321 <ilyaankhan342@gmail.com>
2026-02-18 14:49:44 +0500

    created a interited class and used to display content

--- week 5/class_4.py
+++ week 5/class_4.py
@@ -14,9 +14,16 @@ class Ilyan(Person):       #inherited fron Person
         self.height=height
     def display(self):
         print("Fron ilyan class",self.name,self.age,self.height)
+
+class Student(Person):
+    def display(self):
+        print("From Student class",self.name,self.age)
+
     
 
-p1=Person("Ilyan",19)
-p1.display()
-p2=Ilyan("Damon",165,7)
-p2.display()
+# p1=Person("Ilyan",19)
+# p1.display()
+# p2=Ilyan("Damon",165,7)
+# p2.display()
+p3=Student("Ilyan",19)
+p3.display()
