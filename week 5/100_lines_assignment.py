322368d1c49a5d5680caf03835ebe4c34dce2340
Ilyan321 <ilyaankhan342@gmail.com>
2026-02-20 20:27:01 +0500

    Improved task manager from list of strings to list of objects

--- week 5/100_lines_assignment.py
+++ week 5/100_lines_assignment.py
@@ -1,39 +1,75 @@
-class task:
+class Task:
     name=""
-    due_date=0
+    due_date=""
     completed=False
-    mark_complete=False
+    def __init__(self,name,due_date):
+        self.name=name
+        self.due_date=due_date
+
+    def __str__(self):
+        return f"Task: {self.name}, Due Date: {self.due_date}, Completed: {self.completed}"
 
 
-class task_manager:
+class task_manager(Task):
+
     def __init__(self):
         self.tasks_list=[]
 
+# load tasks from csv
+    def load_tasks(self):
+        try:
+            with open("tasks.csv",'r') as file:
+                for line in file:
+                    task=line.strip().split(',')
+                    new_task=Task(task[0],task[1])
+                    self.tasks_list.append(task)
+        except FileNotFoundError:
+            print("No existing file found. Starting with an empty list here.")
+    
+    #add task to list
     def add_task(self):
         task=input("Enter the task: ")
-        self.tasks_list.append(task)
-        print("Task",task,"has been added.")
-
+        date=input("Enter the due date (YYYY-MM-DD): ")
+        obj=Task(task,date)
+        self.tasks_list.append(obj)
+        print("Task",task,"has been added with due date",date)
+        
+    
+    #view all tasks
     def view_tasks(self):
         if not self.tasks_list:
             print("No task found.")
         else:
-            print("Tasks: ",self.tasks_list)
-        
+            print("Tasks: ")
+            for i in self.tasks_list:
+                print(i)
+            print()
+    
+    #mark as complete and remove file from list
     def mark_task_complete(self):
         task=input("Enter the task to mark as complete: ")
-        if task in self.tasks_list:
-            self.tasks_list.remove(task)
-            print("Task",task,"has been marked as complete.")
-        else:
-            print("Task not found.")
+        found=False
+        for i in self.tasks_list:
 
+            if i.name ==task:
+                self.tasks_list.remove(i)
+                print("Task",task,"has been marked as complete.")
+                found=True
+                break
+        if not found:
+                print("Task not found.")
 
+    # searching task
     def search_task(self):
-        task=input("Enter the task to search: ")
-        if task in self.tasks_list:
-            print("Task",task,"found.")
-        else:
+        search=input("Enter the task to search: ")
+        found=False
+        for task in self.tasks_list:
+            if task.name==search:
+                print("Task found: ",task)
+                found=True
+                break
+            
+        if not found:
             print("Task not found.")
 
 
@@ -41,16 +77,17 @@ class task_manager:
     def save_tasks(self):
         with open("tasks.csv","a") as file:
             for i in self.tasks_list:
-                file.write(i+"\n")
+                if isinstance(i,Task):
+                    file.write(f"{i.name},{i.due_date},{i.completed}\n")
 
 task_manager=task_manager()
+task_manager.load_tasks()
 while True:
     print("1. Add Task")
     print("2. View Tasks")
     print("3. Mark Task as Complete")
     print("4. Search Task")
     print("5. Save and Exit")
-    print("6. Exit")
     choice=int(input("Enter your choice: "))
     if choice==1:
         task_manager.add_task()
@@ -63,8 +100,6 @@ while True:
     elif choice==5:
         task_manager.save_tasks()
         print("Tasks saved to tasks.csv. Exiting...")
-        break
-    elif choice==5:
         break
     else:
         print("Invalid choice. Please try again.")
